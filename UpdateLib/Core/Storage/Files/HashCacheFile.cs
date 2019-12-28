2b29c673dd5f65b364d2862c0df2ad79b104b079
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2019-12-28 16:23:21 +0100

    Add Cache Storage Tests

--- UpdateLib/Core/Storage/Files/HashCacheFile.cs
+++ UpdateLib/Core/Storage/Files/HashCacheFile.cs
@@ -6,5 +6,10 @@ namespace UpdateLib.Core.Storage.Files
     public class HashCacheFile
     {
         public List<HashCacheEntry> Entries { get; set; }
+
+        public HashCacheFile()
+        {
+            Entries = new List<HashCacheEntry>();
+        }
     }
 }
